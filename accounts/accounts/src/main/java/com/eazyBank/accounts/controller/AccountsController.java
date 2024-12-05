84391fbeda1c9ea973b2ab005b3310c9def4fd31
CodingBholenath <Shruti.Puri@gds.ey.com>
2024-12-05 16:20:07 +0530

    Updated folder name

---
+++ accounts/accounts/src/main/java/com/eazyBank/accounts/controller/AccountsController.java
@@ -0,0 +1,160 @@
+package com.eazyBank.accounts.controller;
+
+import com.eazyBank.accounts.constants.AccountsConstants;
+import com.eazyBank.accounts.dto.CustomerDto;
+import com.eazyBank.accounts.dto.ErrorResponseDto;
+import com.eazyBank.accounts.dto.ResponseDto;
+import com.eazyBank.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/api",produces= MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+/*
+With this allargsConstructor AccountsController
+is going to have the constructor that accpets
+the IAccountService as the parameter
+ */
+@Validated
+@Tag(
+        name="CRUD REST API for Accounts in EazyBank",
+        description = "CRUD REST API for Accounts in EazyBank to CREATE,UPDATE,FETCH AND DELETE account details")
+public class AccountsController {
+
+//   @GetMapping("/sayHello")
+//    public String sayHello(){
+//       return "Hello World";
+//   }
+    private IAccountsService iAccountsService;
+@Operation(summary = "Create a new account Rest api",
+        description = "REST API to Create a new  customer & account inside EazyBank")
+@ApiResponses({
+        @ApiResponse(
+                responseCode = "201",
+                description = " HTTP Status CREATED"),
+        @ApiResponse(
+                responseCode = "500",
+                description = "HTTP Status Internal Server Error",
+                content = @Content(
+                    schema = @Schema(implementation = ErrorResponseDto.class)
+                )
+        )
+})
+
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto){
+iAccountsService.createAccount(customerDto);
+    return ResponseEntity.
+            status(HttpStatus.CREATED)
+            .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+}
+@Operation(summary = "Fetch account details Rest api",
+        description = "REST API to fetch account details inside EazyBank")
+@ApiResponses({
+        @ApiResponse(
+                responseCode = "200", description = " HTTP Status OK"),
+        @ApiResponse(
+                responseCode = "500",
+                description = "HTTP Status Internal Server Error",
+                content = @Content(
+                        schema = @Schema(implementation = ErrorResponseDto.class)
+                )
+        )
+})
+@GetMapping("/fetch")
+public ResponseEntity<CustomerDto>fetchAccountDetails(@RequestParam @Pattern(regexp = "[0-9]{10}",message = "The length of number should be 10")
+                                                      String mobileNumber) {
+CustomerDto customerDto=iAccountsService.fetchAccount(mobileNumber);
+
+return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+
+
+}
+@Operation(summary = "Update account details Rest api",
+        description = "REST API to update account details inside EazyBank")
+@ApiResponses({
+@ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status OK"
+),
+        @ApiResponse(
+                responseCode = "417",
+                description = "Exception failed to update account details"
+        ),
+@ApiResponse(
+        responseCode = "500",
+        description = "HTTP Status Internal Server Error",
+        content = @Content(
+                schema =   @Schema(implementation = ErrorResponseDto.class)
+        )
+)
+})
+@PutMapping("/update")
+    public ResponseEntity<ResponseDto>updateAccountDetails(@Valid @RequestBody CustomerDto customerDto){
+
+        boolean isUpdated=iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200,AccountsConstants.MESSAGE_200));
+        }
+        else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+
+        }
+}
+@Operation(summary = "Delete account details Rest api",
+        description = "REST API to delete account details inside EazyBank")
+@ApiResponses({
+@ApiResponse(
+        responseCode ="200",
+        description = "HTTP Status OK"
+),
+        @ApiResponse(
+                responseCode = "417",
+                description = "Exception failed to delete account details"
+        ),
+        @ApiResponse
+                (
+                        responseCode = "500",
+                        description = "HTTP Status Internal Server Error",
+                        content = @Content(
+                                schema =   @Schema(implementation = ErrorResponseDto.class)
+                        )
+                )
+
+
+})
+@DeleteMapping("/delete")
+
+    public ResponseEntity<ResponseDto>deleteAccountDetails(@RequestParam  @Pattern(regexp = "[0-9]{10}",message = "The length of number should be 10")
+                                                           String mobileNumber) {
+    boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
+    if (isDeleted) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+    } else {
+        return ResponseEntity
+                .status(HttpStatus.EXPECTATION_FAILED)
+                .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
+    }
+}
+}
