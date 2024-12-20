53a98897dbad0380083291dce5393b899c41511e
Shruti Puri <shruti.puri@gds.ey.com>
2024-12-20 11:32:20 +0530

    Server-side service discovery and load balancing

---
+++ section_17/accounts/src/main/java/com/eazyBank/accounts/service/Impl/AccountsServiceImpl.java
@@ -0,0 +1,139 @@
+package com.eazyBank.accounts.service.Impl;
+
+import com.eazyBank.accounts.constants.AccountsConstants;
+import com.eazyBank.accounts.dto.AccountsDto;
+import com.eazyBank.accounts.dto.AccountsMsgDto;
+import com.eazyBank.accounts.dto.CustomerDto;
+import com.eazyBank.accounts.entity.Accounts;
+import com.eazyBank.accounts.entity.Customer;
+import com.eazyBank.accounts.exception.CustomerAlreadyExistsException;
+import com.eazyBank.accounts.exception.ResouceNotFoundException;
+import com.eazyBank.accounts.mapper.AccountsMapper;
+import com.eazyBank.accounts.mapper.CustomerMapper;
+import com.eazyBank.accounts.repository.AccountRepository;
+import com.eazyBank.accounts.repository.CustomerRepository;
+import com.eazyBank.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class AccountsServiceImpl  implements IAccountsService {
+    private static final Logger log = LoggerFactory.getLogger(AccountsServiceImpl.class);
+private AccountRepository accountRepository;
+private CustomerRepository customerRepository;
+private final StreamBridge streamBridge;
+    @Override
+    public void createAccount(CustomerDto customerDto) {
+Customer customer= CustomerMapper.mapToCustomer(customerDto, new Customer());
+Optional<Customer> optionalCustomer=customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+if(optionalCustomer.isPresent())
+    throw new CustomerAlreadyExistsException("Customer already exists with the given mobile number"+customerDto.getMobileNumber()); //customer already exists(Optional)
+//customer.setCreatedAt(LocalDateTime.now());
+//        customer.setCreatedBy("Anonymous");
+        Customer savedCustomer=customerRepository.save(customer);
+Accounts savedAccount=accountRepository.save(createNewAccount(savedCustomer));
+sendCommunication(savedAccount,savedCustomer);
+    }
+    private void sendCommunication(Accounts account, Customer customer) {
+        var accountsMsgDto = new AccountsMsgDto(account.getAccountNumber(), customer.getName(),
+                customer.getEmail(), customer.getMobileNumber());
+        log.info("Sending Communication request for the details: {}", accountsMsgDto);
+        var result = streamBridge.send("sendCommunication-out-0", accountsMsgDto);
+        log.info("Is the Communication request successfully triggered ? : {}", result);
+    }
+
+
+
+    /**
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+//        newAccount.setCreatedAt(LocalDateTime.now());
+//        newAccount.setCreatedBy("Anonymous");
+        return newAccount;
+    }
+    @Override
+    public CustomerDto fetchAccount(String mobileNumber) {
+     Customer customer=  customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+               ()->new ResouceNotFoundException("Customer","Mobile Number",mobileNumber)
+       );
+     Accounts accounts=accountRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+             ()->new ResouceNotFoundException("Account","customerId",customer.getCustomerId().toString())
+     );
+     CustomerDto customerDto=  CustomerMapper.mapToCustomerDto(customer,new CustomerDto());
+      customerDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts,new AccountsDto()));
+        return customerDto;
+    }
+
+    @Override
+    public boolean updateAccount(CustomerDto customerDto) {
+      boolean isUpdated=false;
+      AccountsDto accountsDto=customerDto.getAccountsDto();
+
+      if(accountsDto!=null) {
+          Accounts accounts = accountRepository.findById(accountsDto.getAccountNumber()).orElseThrow(
+                  () -> new ResouceNotFoundException("Accounts", "AccountNumber", accountsDto.getAccountNumber().toString())
+
+          );
+          AccountsMapper.mapToAccounts(accountsDto, accounts);
+          accounts = accountRepository.save(accounts);
+
+          Long customerId = accounts.getCustomerId();
+          Customer customer = customerRepository.findById(customerId).orElseThrow(
+                  () -> new ResouceNotFoundException("Customer", "CustomerId", customerId.toString())
+          );
+          CustomerMapper.mapToCustomer(customerDto, customer);
+          customerRepository.save(customer);
+          isUpdated = true;
+      }
+          return isUpdated;
+
+
+
+    }
+
+    @Override
+    public boolean deleteAccount(String mobileNumber) {
+        Customer customer=customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                ()->new ResouceNotFoundException("Customer","mobileNumber",mobileNumber)
+        );
+        //since customer id is not primary key in account table we have to write this method inside account repository
+        accountRepository.deleteByCustomerId(customer.getCustomerId());
+        customerRepository.deleteById(customer.getCustomerId());
+
+        return true;
+    }
+
+    @Override
+    public boolean udpdateCommuicationStatus(Long accountNumber) {
+        boolean isUpdated=false;
+        if(accountNumber!=null){
+            Accounts accounts=accountRepository.findById(accountNumber).orElseThrow(
+                    ()-> new ResouceNotFoundException("Accounts","AccountNumber",accountNumber.toString())
+            );
+            accounts.setCommuicationSw(true);
+            accountRepository.save(accounts);
+            isUpdated=true;
+        }
+        return isUpdated;
+    }
+
+
+}
---
+++ section_17/loans/src/main/java/com/eazyByte/loans/LoansApplication.java
@@ -0,0 +1,36 @@
+package com.eazyByte.loans;
+
+import com.eazyByte.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.persistence.Entity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "EazyBank Loans microservice REST API Documentation",
+				version = "v3",
+				contact = @Contact(name = "Shruti",
+						email = "shrutipuri13224@gmail.com",
+						url = "https://www.eazybytes.com"
+				)
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
