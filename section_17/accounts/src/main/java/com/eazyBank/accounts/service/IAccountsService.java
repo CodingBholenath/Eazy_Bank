7025b096f495b685297fae8d72bf57fb049d7c79
Shruti Puri <shruti.puri@gds.ey.com>
2024-12-20 10:54:09 +0530

    Deep Dive on Helm

---
+++ section_17/accounts/src/main/java/com/eazyBank/accounts/service/IAccountsService.java
@@ -0,0 +1,33 @@
+package com.eazyBank.accounts.service;
+
+import com.eazyBank.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+    /**
+     *This method
+     *is accepecting@param CustomerDto-CustomerDtoObject
+     */
+    void createAccount(CustomerDto customerDto);
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Accounts Details based on a given mobileNumber
+     */
+     CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+
+    /**
+     * delete -@param mobile number
+     *
+     */
+    boolean deleteAccount(String mobileNumber);
+
+    boolean udpdateCommuicationStatus(Long accountNumber);
+}
